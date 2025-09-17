0aef19351a98a52e1595cc99a2596a468312f803
theBappy <abappy575@gmail.com>
2025-09-17 19:01:58 +0600

    daily challenge solved

--- daily_challenges/2197.py
+++ daily_challenges/2197.py
@@ -15,3 +15,20 @@ class Solution:
                 num = LCM
             result.append(num)
         return num
+
+# from math import gcd
+# class Solution:
+#     def replaceNonCoprimes(self, nums):
+#         result = []
+#         for num in nums:
+#             while result:
+#                 prev = result[-1]
+#                 curr = num
+#                 GCD = gcd(prev, num)
+#                 if GCD == 1:
+#                     break
+#                 result.pop()
+#                 LCM = prev // GCD * curr
+#                 num = LCM
+#             result.append(num)
+#         return num
