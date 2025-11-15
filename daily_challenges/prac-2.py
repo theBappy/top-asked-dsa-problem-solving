b47d3a29daf438e9f15bffe80721894c15d71cba
theBappy <abappy575@gmail.com>
2025-11-15 18:45:27 +0600

    refactor

--- daily_challenges/prac-2.py
+++ daily_challenges/prac-2.py
@@ -1,6 +1,6 @@
 # Daily Challenges: 474, 2654, 3228, 2536
 
-# Tress: 938, 530, 501,1038, 1382, 623
+# Tress: 938,530,501,1038,1382,623
 from collections import defaultdict, deque, Counter
 from typing import List
 
